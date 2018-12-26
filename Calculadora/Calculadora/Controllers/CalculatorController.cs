51111f8474e4b7f719ae809d728422905e5820d3
Juliano da Silva Barbosa <julianosbarbosa@hotmail.com>
2018-12-26 00:56:13 -0200

    UPDATE - implement 4 operations basics

--- Calculadora/Calculadora/Controllers/CalculatorController.cs
+++ Calculadora/Calculadora/Controllers/CalculatorController.cs
@@ -9,13 +9,56 @@ namespace Calculadora.Controllers
     [Route("api/[controller]")]
     public class CalculatorController : Controller
     {
-        // GET api/calculator/5/5
-        [HttpGet("{firstNumber}/{secondNumber}")]
+        // GET api/calculator/divi/5/5
+        [HttpGet("divi/{firstNumber}/{secondNumber}")]
+        public IActionResult Divi (string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var divi = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+
+                return Ok(divi.ToString());
+            }
+
+            return BadRequest("value");
+        }
+
+        // GET api/calculator/mult/5/5
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        public IActionResult Mult(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var mult = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+
+                return Ok(mult.ToString());
+            }
+
+            return BadRequest("value");
+        }
+
+        // GET api/calculator/minus/5/5
+        [HttpGet("minus/{firstNumber}/{secondNumber}")]
+        public IActionResult Minus(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var minus = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+
+                return Ok(minus.ToString());
+            }
+
+            return BadRequest("value");
+        }
+
+        // GET api/calculator/sum/5/5
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
                 var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+
                 return Ok(sum.ToString());
             }
 
